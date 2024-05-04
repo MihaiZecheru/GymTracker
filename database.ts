3987a2143a36f2575a5a4f1ff96e85511d336c02
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-04 01:15:39 -0700

    Fix Database.AddEntry

--- database.ts
+++ database.ts
@@ -17,23 +17,41 @@ function MakeEntry(reps: number, weight_per_rep: number, date: DateInMS): IEntry
  * Database schema consists of a table of users, a table of entries, and a table of entry collections.
  **/
 export default abstract class Database {
-  private static db = new sqlite3.Database('./gym.db');
+  private static db = new sqlite3.Database('./gym.db')!;
 
   /**
    * Get a user from the database
    */
-	public static GetUser(user_id: UserID): IUser {
-		// TODO: finish
-    return {} as IUser;
+	public static async GetUser(user_id: UserID): Promise<IUser | null> {
+		return new Promise((resolve) => {
+      this.db.get(`SELECT * FROM Users WHERE user_id = ?`, user_id, (err, row) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+  
+        resolve(row as IUser | null);
+      });
+    });
 	}
 
   /**
    * Get an entry collection from the database
    * @param excersize The excersize the entry collection represents
    */
-	public static GetEntryCollection(user_id: UserID, excersize: TExcersize): IEntryCollection {
-		// TODO: finish
-    return {} as IEntryCollection;
+	public static async GetEntryCollection(user_id: UserID, excersize: TExcersize): Promise<IEntryCollection> {
+		return new Promise((resolve) => {
+      this.db.get(`SELECT * FROM ExcersizeEntryCollection WHERE user_id = ? AND excersize = ?`, user_id, excersize, (err: Error | null, row: unknown) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+
+        // @ts-ignore
+        if (row) row.entries = JSON.parse(row.entries);
+        resolve(row as IEntryCollection);
+      });
+    });
 	}
 
   /**
@@ -41,11 +59,38 @@ export default abstract class Database {
    * @param excersize The excersize the entry collection represents
    * @param entry_id The ID of the entry to add to the collection. It must already exist in the database.
    */
-	public static UpdateEntryCollection(user_id: UserID, excersize: TExcersize, entry_id: EntryID): void {
-		const entry_collection = this.GetEntryCollection(user_id, excersize);
+	public static async UpdateEntryCollection(user_id: UserID, excersize: TExcersize, entry_id: EntryID): Promise<void> {
+		const entry_collection = await this.GetEntryCollection(user_id, excersize);
     entry_collection.entries.push(entry_id);
-    // TODO: update in DB
+    
+    return new Promise((resolve) => {
+      this.db.run(`UPDATE ExcersizeEntryCollection SET entries = ? WHERE user_id = ? AND excersize = ?`, JSON.stringify(entry_collection), user_id, excersize, (err: Error | null) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+
+        resolve();
+      });
+    });
 	}
+
+  /**
+   * Create a new entry collection object in the database
+   * @param excersize The excersize to create an entry collection for
+   */
+  public static async MakeEntryCollection(user_id: UserID, excersize: TExcersize): Promise<void> {
+    return new Promise((resolve) => {
+      this.db.run(`INSERT INTO ExcersizeEntryCollection (user_id, excersize, entries) VALUES (?, ?, ?)`, user_id, excersize, JSON.stringify([]), (err: Error | null) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+
+        resolve();
+      });
+    });
+  }
 
   /**
    * Add an entry to the database and update the entry collection it belongs to
@@ -53,9 +98,12 @@ export default abstract class Database {
    * @param reps The amount of reps that were done (int)
    * @param weight_per_rep The amount of weight that was lifted per rep (float)
    */
-	public static AddEntry(user_id: UserID, excersize: TExcersize, reps: number, weight_per_rep: number): void {
-		const entry = MakeEntry(reps, weight_per_rep, Date.now());
-		let entry_collection: IEntryCollection = this.GetEntryCollection(user_id, excersize);
+	public static async AddEntry(user_id: UserID, excersize: TExcersize, reps: number, weight_per_rep: number): Promise<void> {
+		const entry: IEntry = MakeEntry(reps, weight_per_rep, Date.now());
+    await this.PostEntryToDB(entry);
+		let entry_collection: IEntryCollection = await this.GetEntryCollection(user_id, excersize);
+    if (!entry_collection) this.MakeEntryCollection(user_id, excersize);
 		this.UpdateEntryCollection(user_id, excersize, entry.entry_id);
 	}
 }
+Database.GetUser("5qJjkBYh" as UserID).then(console.log);
