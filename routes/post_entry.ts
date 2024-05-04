913c98ad77b571128bad720c5f1ef7992ef5408e
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-04 00:09:01 -0700

    Finish post_entry route

--- routes/post_entry.ts
+++ routes/post_entry.ts
@@ -1,4 +1,26 @@
+import Database from "../database.ts";
+import Excersizes from "../scripts/excersizes.js";
+import { TExcersize, UserID } from "../types";
+import Validate from "../validate.ts";
+
 export default function post_entry(req: any, res: any): void {
-  res.status(200).send({ success: true });
-  // res.status(400).send({ success: false });
+  const user_id = req.params.user_id;
+  const excersize = req.params.excersize;
+
+  if (Database.GetUser(user_id) === null)
+    return res.status(400).send({ success: false, error: `User with user_id "${user_id}" does not exist` });
+
+  if (!(excersize in Excersizes))
+    return res.status(400).send({ success: false, error: `Excersize "${excersize}" does not exist` });
+
+  const reps = req.body?.reps;
+  const weight = req.body?.weight;
+
+  if (Validate(user_id, res)) return;
+  if (Validate(excersize, res)) return;
+  if (Validate(reps, res)) return;
+  if (Validate(weight, res)) return;
+
+  Database.AddEntry(user_id as UserID, excersize as TExcersize, reps, weight);
+  return res.status(200).send({ success: true });
 }
