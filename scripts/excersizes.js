e3a28431dbf921118b6785b9e7380c71e8c11b24
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-04 00:01:13 -0700

    Excersize list for index.html

---
+++ scripts/excersizes.js
@@ -0,0 +1,5 @@
+/**
+ * List of excersizes from the TExcersize type for use in the frontend
+ */
+const Excersizes = ["Bench - Arms", "Bicep Curls - Arms", "Treadmill - Cardio"]; // TODO: add the rest
+export default Excersizes;
