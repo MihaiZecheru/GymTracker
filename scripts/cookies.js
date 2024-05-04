ef4d18385d30779105833f4a8bbbd85830754fcd
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-03 22:36:33 -0700

    Formatting

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -2,9 +2,9 @@ export function addCookie(name, value, days) {
   let expires = "";
   
   if (days) {
-      let date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
+    let date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
   }
 
   document.cookie = name + "=" + value + expires + "; path=/";
@@ -18,9 +18,8 @@ export function getCookie(name) {
   const cookies = document.cookie.split(';');
   
   for (let i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i].trim();
-      if (cookie.startsWith(name + '='))
-          return cookie.substring(name.length + 1);
+    let cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) return cookie.substring(name.length + 1);
   }
 
   return null;
