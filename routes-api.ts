69bf12e87bb440c6af75bfd3535b55208cdcf173
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-19 23:42:55 -0700

    Add get_user_history function

--- routes-api.ts
+++ routes-api.ts
@@ -3,13 +3,15 @@ import get_user_by_id from "./routes/get_user_by_id";
 import post_entry from "./routes/post_entry";
 import make_graph from "./routes/make_graph";
 import get_simplified_excersizes from "./routes/get_simplified_excersizes";
+import get_user_history from "./routes/get_user_history";
 
 const API = {
   get_user_by_id,
   get_entry_collection,
   post_entry,
   make_graph,
-  get_simplified_excersizes
+  get_simplified_excersizes,
+  get_user_history
 }
 
 export default API;
