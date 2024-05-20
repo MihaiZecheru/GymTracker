82c748dceff15a5cc7ddaeef4c5af5b4b283fefd
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-20 01:55:59 -0700

    Fix format_date for linux terminal

--- routes/get_user_history.ts
+++ routes/get_user_history.ts
@@ -6,8 +6,12 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function format_date(date: number) {
-  const date_obj = new Date(date - 25200);
-  return `${weekdays[date_obj.getDay()]}, ${months[date_obj.getMonth()]} ${date_obj.getDate()}, ${date_obj.getFullYear()}`;
+  const date_info = new Date(date).toLocaleDateString("en-US", { timeZone: "America/Los_Angeles" }).split('/');
+  const day = date_info[1];
+  const month = months[parseInt(date_info[0]) - 1];
+  const year = date_info[2];
+  const weekday = weekdays[new Date(date_info.join("/")).getDay()];
+  return `${weekday}, ${month} ${day}, ${year}`;
 }
 
 export default function get_user_history(req: any, res: any): void {
